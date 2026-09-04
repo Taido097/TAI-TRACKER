4f8836452b87bc8c15b663a57af3e36115b653e2
Taido097 <taido097@gmail.com>
2026-09-04 10:00:35 -0700

    Add new client button to clients page

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -1,2 +1,15 @@
+'use client';
+import { useState } from 'react';
 import { ClientsPage } from '@/components/table-pages';
-export default function Page(){return <ClientsPage/>}
+import { AddClientDialog } from '@/components/dialogs';
+
+export default function Page(){
+  const [adding,setAdding]=useState(false);
+  return <>
+    <div style={{display:'flex',justifyContent:'flex-end',marginBottom:12}}>
+      <button className="btn accent" onClick={()=>setAdding(true)}>+ Add Client</button>
+    </div>
+    <ClientsPage/>
+    {adding&&<AddClientDialog close={()=>setAdding(false)}/>} 
+  </>
+}
